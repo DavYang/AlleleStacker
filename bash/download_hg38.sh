6b52e50840d52b7a9ae32897f9125fc8ccde6f08
DavYang <99273325+DavYang@users.noreply.github.com>
2025-02-03 10:58:05 -0500

    Create download_hg38.sh

---
+++ bash/download_hg38.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=hg38_download
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --mem=64gb
+#SBATCH --output=download.%A_%a.out
+
+
+BASE_URL="https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome"
+
+wget "${BASE_URL}/GRCh38_full_analysis_set_plus_decoy_hla.fa"
+wget "${BASE_URL}/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai" 
+
+echo "done"
