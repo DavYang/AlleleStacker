ab4c75fc7b7c28160af8f1dcaf1dd67c29679d54
DavYang <99273325+DavYang@users.noreply.github.com>
2025-02-03 11:02:41 -0500

    Update download_hg38.sh

--- bash/download_hg38.sh
+++ bash/download_hg38.sh
@@ -7,6 +7,9 @@
 #SBATCH --output=download.%A_%a.out
 
 
+mkidir -p /reference_genomes/hg38
+cd /reference_genomes/hg38
+
 BASE_URL="https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome"
 
 wget "${BASE_URL}/GRCh38_full_analysis_set_plus_decoy_hla.fa"
